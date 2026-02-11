acac72495a8b4d413cbaa3ed0d47a1e344e8a47e
Andres Geovanny Diaz Guaje <diazguaje@gmail.com>
2026-02-11 14:51:31 -0500

    fix: resolve SQLA

--- app.py
+++ app.py
@@ -76,9 +76,9 @@ def create_default_admin():
     finally:
         db.close()
 
+# Crear instancia de app para gunicorn (Render)
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
-
-#function to validate password strengh
